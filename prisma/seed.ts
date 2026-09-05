1c7e560e8fdee36ace4a7e77e85748909b4edf38
om <omtiw1209@gmail.com>
2026-09-05 16:48:12 +0530

    prisma is global now xo

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import "dotenv/config";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-const prisma = new PrismaClient({
-  adapter,
-});
+import { prisma } from "../lib/prisma";
 
 async function main() {
   await prisma.college.createMany({
